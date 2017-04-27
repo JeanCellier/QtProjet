3b87289b8b6fdc9e78fbcc0dd681d16a3095b425
Pierre <pierre.fervault@laposte.net>
2017-04-27 10:15:03 +0200

    Ajout treeView

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include "addhealthcareoperator.h"
 #include "about.h"
 #include "QStandardItemModel"
+#include "QFileSystemModel"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,6 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->statusBar()->showMessage("Vous venez de vous connecter");
+
+    update_patientSearchTableView();
+    update_ressourceTreeView();
 }
 
 MainWindow::~MainWindow()
@@ -68,20 +72,54 @@ void MainWindow::on_actionAjouterPersonnelSoins_triggered()
     }
 }
 
+void MainWindow::update_patientSearchTableView(){
+    QStringList listeNom;
+    listeNom << "Nom" << "Prenom" << "Adresse" << "Ville" << "Code Postal" << "Durée de consultation"
+             << "Jour de consultation" << "Priorité" << "Ressources";
+    QStandardItemModel * standardItemModel = new QStandardItemModel(0,9,0);
+    standardItemModel->setHorizontalHeaderLabels(listeNom);
+    this->ui->patientSearchTableView->setModel(standardItemModel);
+}
 
+void MainWindow::update_ressourceTreeView()
+{
+    QStandardItemModel * standardModel = new QStandardItemModel();
+    QStandardItem *rootNode = standardModel->invisibleRootItem();
+
+
+    //defining a couple of items
+    QStandardItem *doctorAItem = new QStandardItem("Docteur A");
+    QStandardItem *doctorBItem = new QStandardItem("Docteur B");
+    QStandardItem *radioItem = new QStandardItem("Radiologue");
+    QStandardItem *nurseItem = new QStandardItem("Infirmiere");
+    QStandardItem *kineItem = new QStandardItem("Kinesitherapeute");
+    QStandardItem *psyItem = new QStandardItem("Psychologue");
+
+    QStandardItem *jeanItem = new QStandardItem("Jean");
+    QStandardItem *pierreItem = new QStandardItem("Pierre");
+
+    //building up the hierarchy
+    rootNode-> appendRow(doctorAItem);
+    rootNode-> appendRow(doctorBItem);
+    rootNode-> appendRow(radioItem);
+    rootNode-> appendRow(nurseItem);
+    rootNode-> appendRow(kineItem);
+    rootNode-> appendRow(psyItem);
+
+    doctorAItem -> appendRow(jeanItem);
+    psyItem -> appendRow(pierreItem);
+
+    //register the model
+    this->ui->ressourceTreeView->setModel(standardModel);
+    this->ui->ressourceTreeView->expandAll();
+}
 
 void MainWindow::on_toolBox_currentChanged(int index)
 {
     if (index==0){
-        QStringList listeNom;
-        listeNom << "Nom" << "Prenom" << "Adresse" << "Ville" << "Code Postal" << "Durée de consultation"
-                 << "Jour de consultation" << "Priorité" << "Ressources";
-        QStandardItemModel * standardItemModel = new QStandardItemModel(0,9,0);
-        standardItemModel->setHorizontalHeaderLabels(listeNom);
-        this->ui->patientSearchTableView->setModel(standardItemModel);
-        this->statusBar()->showMessage("0");
+        update_patientSearchTableView();
     }
     if (index==1){
-        this->statusBar()->showMessage("1");
+        update_ressourceTreeView();
     }
 }
